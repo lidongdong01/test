49d79c96dd5c505a872aac2be4775cd7fbb2903e
lidongdong01 <lidongdong01@baidu.com>
2021-01-26 11:20:33 +0800

    test2

--- MTJ/BaiduMobStat.h
+++ MTJ/BaiduMobStat.h
@@ -8,9 +8,6 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#ifdef __IPHONE_8_0
-#import <WebKit/WebKit.h>
-#endif
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -34,33 +31,8 @@ typedef enum _BaiduMobStatPushPlatform{
 } BaiduMobStatPushPlatform;
 
 /**
- 信息流监控策略
- */
-typedef enum _BaiduMobStatFeedTrackStrategy {
-    BaiduMobStatFeedTrackStrategyAll = 0,       //全部监控
-    BaiduMobStatFeedTrackStrategySingle = 1,    //单个监控，需要监控的tableviw需要设置enableListTrack属性为YES
-    BaiduMobStatFeedTrackStrategyNone = 2,      //关闭feed监控，不监控任何列表
-} BaiduMobStatFeedTrackStrategy;
-
-/**
- 全局扩展信息
- */
-@interface BaiduMobStatExtraInfo : NSObject
-@property (nonatomic, copy, nullable) NSString* v1;
-@property (nonatomic, copy, nullable) NSString* v2;
-@property (nonatomic, copy, nullable) NSString* v3;
-@property (nonatomic, copy, nullable) NSString* v4;
-@property (nonatomic, copy, nullable) NSString* v5;
-@property (nonatomic, copy, nullable) NSString* v6;
-@property (nonatomic, copy, nullable) NSString* v7;
-@property (nonatomic, copy, nullable) NSString* v8;
-@property (nonatomic, copy, nullable) NSString* v9;
-@property (nonatomic, copy, nullable) NSString* v10;
-@end
-
-/**
  百度移动应用统计接口
- 当前版本 5.3.0_18
+ 当前版本 5.2.7_0
  */
 @interface BaiduMobStat : NSObject
 /**
@@ -134,59 +106,6 @@ typedef enum _BaiduMobStatFeedTrackStrategy {
 @property (nonatomic, assign) BOOL enableGps;
 
 /**
- 设置是否允许监控webview（V4.6.7新增）
- 默认为YES
- */
-@property (nonatomic, assign) BOOL enableWebViewAutoTrack;
-
-/**
- 当enableWebViewAutoTrack属性设置为NO时，该接口无效，可忽略。
- 当多个webview的delegate类存在继承关系时，为了避免因调用super方法而导致的死循环风险，可以设置是否只统计子类Delegate（V4.9.5新增）
- 备注：在webview的delegate方法中没有调用super方法的情况下，不受影响。此时为了保证数据完整，不建议设置为YES。
- 默认为NO
- */
-@property (nonatomic, assign) BOOL onlyTrackSubClassOfWebDelegate;
-
-/**
- 设置UIViewController是否监控（V4.6.7新增）
- 默认为YES
- */
-@property (nonatomic, assign) BOOL enableViewControllerAutoTrack;
-
-/**
- 设置是否优先使用“类名”进行UIViewController的页面统计（V5.0.0新增）
- 默认为NO，即优先使用页面title进行统计，没有title情况下再使用类名统计
- （推荐）设置为YES，即使用类名进行页面统计，统计效果更准确、聚合度更高
- */
-@property (nonatomic, assign) BOOL trackViewControllerWithClassName;
-
-/**
- 设置控件点击event事件是否监控（V4.8.3新增）
- 默认为YES
- */
-@property (nonatomic, assign) BOOL enableEventAutoTrack;
-
-/**
- 设置是否允许推送文案的采集，用于启动来源分析（V4.9.5新增）
- 默认为YES
- */
-@property (nonatomic, assign) BOOL enableGetPushContent;
-
-/**
- 设置TableView监控策略（V4.8.0新增）
- 默认为BaiduMobStatFeedTrackStrategyAll，全部监控
- 设置为BaiduMobStatFeedTrackStrategySingle单个监控，则要监控的tableview需要设置enableListTrack属性为YES
- 设置为BaiduMobStatFeedTrackStrategyNone，则不监控任何列表
- */
-@property (nonatomic, assign) BaiduMobStatFeedTrackStrategy feedTrackStrategy;
-
-/**
- 设置是否使用启动来源分析功能（V4.9.5新增）
- 默认为YES
- */
-@property (nonatomic, assign) BOOL enableLaunchRefererTrack;
-
-/**
  设置Crash日志中附带的信息
  长度限制256字节，超出截断
  */
@@ -223,18 +142,6 @@ typedef enum _BaiduMobStatFeedTrackStrategy {
 - (void)setPageProperty:(nullable NSDictionary *)pageProperty;
 
 /**
- 分析云使用，添加全埋点事件属性。
- 生效范围: 设置后下一次全埋点事件会带该属性
- 传入nil，可清空标记。
- key值为用户提前在网站创建的“属性名称”，若没有提前创建，则无统计效果。
- setSessionProperty、setPageProperty、setAutoEventProperty、setUserProperty共用属性个数，最多传入100个key-value值，超出部分无效
- 每个value长度限制256字节,String格式传入，如果为是非类型，则传入'1' '0'、如果是时间信息需要转为时间戳的string格式
- 如果没有设置autoEventProperty，页面属性默认使用session属性
- 调用例子见demo工程
- */
-- (void)setAutoEventProperty:(nullable NSDictionary *)autoEventProperty;
-
-/**
  设置用户自定义的用户属性信息，在startWithAppId之前调用
  设置一次UserPorperty后，属性与该设备绑定。传入新的UserPorperty将替换老的UserPorperty内容。
  传入nil，可清空标记。
@@ -252,20 +159,6 @@ typedef enum _BaiduMobStatFeedTrackStrategy {
  @param appKey 用户在mtj网站上创建应用，获取对应的appKey
  */
 - (void)startWithAppId:(NSString *)appKey;
-
-/**
- 设置全局附加信息
- 此API调用后的日志信息才会有全局附加信息
- 若多次调用，后续调用会覆盖之前的信息，以最后一次调用为准
- 
- @param info 全局附加信息
- */
-- (void)setGlobalExtraInfo:(BaiduMobStatExtraInfo *) info;
-
-/**
- 清空全局附加信息
- */
-- (void)clearGlobalExtraInfo;
 
 /**
  记录一次事件的点击，eventId请在网站上创建。未创建的evenId记录将无效。
@@ -319,7 +212,7 @@ typedef enum _BaiduMobStatFeedTrackStrategy {
  @param eventId 自定义事件Id，提前在网站端创建
  @param attributes 事件属性，对应的key需要在网站上创建，注意：value只接受NSString
  */
-- (void)eventEnd:(NSString *)eventId attributes:(nullable NSDictionary *) attributes;
+- (void)eventEnd:(NSString *)eventId attributes:(nullable NSDictionary *)attributes;
 
 /**
  记录某个页面访问的开始，请参见Example程序，在合适的位置调用。
@@ -346,7 +239,7 @@ typedef enum _BaiduMobStatFeedTrackStrategy {
  @param name WKScriptMessage的name
  @param body WKScriptMessage的body 只接受NSDictionary类型
  */
-- (void)didReceiveScriptMessage:(NSString*)name body:(NSDictionary *)body;
+- (void)didReceiveScriptMessage:(NSString *)name body:(NSDictionary *)body;
 
 /**
  主动上传的Exception信息记录
@@ -370,6 +263,7 @@ typedef enum _BaiduMobStatFeedTrackStrategy {
  */
 - (NSString *)getDeviceCuid;
 
+
 /**
  获取设备的测试ID，同时有Log打印
  返回SDK生成的设备测试ID
@@ -379,23 +273,12 @@ typedef enum _BaiduMobStatFeedTrackStrategy {
 - (NSString *)getTestDeviceId;
 
 /**
- 上传第三方Push平台的Id，pushId长度限制1024字节。设置为nil或者空字符串，则清空对应平台的pushId
+  上传第三方Push平台的Id，pushId长度限制1024字节。设置为nil或者空字符串，则清空对应平台的pushId
  
- @param pushId 从第三方Push SDK接口中获取的pushId
- @param platform 指定Push平台类型，详见枚举声明
+  @param pushId 从第三方Push SDK接口中获取的pushId
+  @param platform 指定Push平台类型，详见枚举声明
  */
 - (void)setPushId:(nullable NSString *)pushId platform:(BaiduMobStatPushPlatform)platform;
-
-/**
- 传入scene:(UIScene *)scene openURLContexts:(NSSet<UIOpenURLContext *> *)URLContexts 中的URLContexts参数
- 与scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions中connectionOptions.URLContexts参数
- 用于统计启动来源、二维码圈选等功能
-
-@param URLContexts scene:openURLContexts:回调函数的参数
-*/
-#ifdef __IPHONE_13_0
-- (void)mtjOpenURLContexts:(NSSet<UIOpenURLContext *> *)URLContexts;
-#endif
 @end
 
 /**
@@ -418,74 +301,6 @@ typedef enum _BaiduMobStatFeedTrackStrategy {
 - (void)logEvent:(NSString *)eventId eventLabel:(NSString *)eventLabel attributes:(nullable NSDictionary *)attributes;
 - (void)logEventWithDurationTime:(NSString *)eventId eventLabel:(NSString *)eventLabel durationTime:(unsigned long)duration attributes:(nullable NSDictionary *)attributes;
 - (void)eventEnd:(NSString *)eventId eventLabel:(NSString *)eventLabel attributes:(nullable NSDictionary *)attributes;
-
-@end
-
-/**
- Category 声明
- */
-@interface UIViewController (BaiduMobStatViewController)
-/**
- 是否禁用自动收集此页面，默认为NO
- */
-@property (nonatomic, assign) BOOL disableBaiduMobAutoRecord;
-
-/**
- 手动设置自动收集的页面名称
- */
-@property (nonatomic, copy, nullable) NSString *titleForBaiduMobStat;
-
-@end
-
-/**
- 百度移动统计UIView Category
- */
-@interface UIView (BaiduMobStatView)
-
-/**
- 手动设置自动收集的附加信息
- 对应的key需要在网站上创建，注意：value只接受NSString
- */
-@property (nonatomic, copy, nullable) NSDictionary *attributesForBaiduMobStat;
-
-@end
-
-/**
- 百度移动统计UITableView Category
- */
-@interface UITableView (BaiduMobStatTableView)
-
-/**
- 设置TableView的栏目名称，为MTJ分析信息流报表中的”栏目名称“,长度限制256字节，超出截断
- 如果不设置，则默认采用tableview所在的Controller title作为“栏目名称”
- 建议在TableView加载前设置。
- */
-@property (nonatomic, copy, nullable) NSString *listName;
-
-/**
- 设置单个TableView是否需要被监控
- 默认情况下，sdk监控全局TableView，不需要设置该接口
- 当设置了全局关闭之后，可以设置单个tableview打开指定的TableView监控
- */
-@property (nonatomic, assign) BOOL enableListTrack;
-
-@end
-
-@interface UITableViewCell (BaiduMobStatTableViewCell)
-
-/**
- 设置Cell的信息Title，为MTJ分析信息流报表中的”信息标题“,长度限制256字节，超出截断
- 如果不设置，则SDK会自动识别一个Title
- 建议在cell加载前设置。
- */
-@property (nonatomic, copy, nullable) NSString *contentTitle;
-
-/**
- 设置Cell的信息ID，是用户自身系统中的内容id,长度限制256字节，超出截断
- 如果不设置，则id为空
- 建议在cell加载前设置。
- */
-@property (nonatomic, copy, nullable) NSString *contentId;
 
 @end
 
