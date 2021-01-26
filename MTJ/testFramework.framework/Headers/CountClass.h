ea066c5621feaebaf9e0492489b1433a1b8cc628
lidongdong01 <lidongdong01@baidu.com>
2021-01-26 10:58:15 +0800

    test

---
+++ MTJ/testFramework.framework/Headers/CountClass.h
@@ -0,0 +1,17 @@
+//
+//  CountClass.h
+//  testFramework
+//
+//  Created by Li,Dongdong(BDG) on 2021/1/13.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CountClass : NSObject
+
+- (void)countTest;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MTJ/testFramework.framework/Headers/testFramework.h
@@ -0,0 +1,18 @@
+//
+//  testFramework.h
+//  testFramework
+//
+//  Created by Li,Dongdong(BDG) on 2021/1/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testFramework.
+FOUNDATION_EXPORT double testFrameworkVersionNumber;
+
+//! Project version string for testFramework.
+FOUNDATION_EXPORT const unsigned char testFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testFramework/PublicHeader.h>
+
+#import <testFramework/CountClass.h>
